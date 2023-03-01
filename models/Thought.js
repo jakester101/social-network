5516e9e4db624027ebc414449a075ce31cfe9af4
jakester101 <55941541+jakester101@users.noreply.github.com>
2023-02-28 23:35:32 -0500

    models set up

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+function formatDate(timestamp) {
+  return new Date(timestamp).toISOString();
+}
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
